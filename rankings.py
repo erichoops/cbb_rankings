a63ea3b21ac670ed0295f510a32d5edd953c9321
Eric Smith <esmith1092@gmail.com>
2026-01-14 00:41:09 -0600

    removing recency to automate rankings

--- rankings.py
+++ rankings.py
@@ -21,36 +21,36 @@ def generate_rankings():
 
 #     soup = BeautifulSoup(html, "html.parser")
 
-    url = f"https://barttorvik.com/?year=2026&sort=&hteam=&t2value=&conlimit=All&state=All&begin={formatted_date}&end=20260501&top=0&revquad=0&quad=5&venue=All&type=All&mingames=0#"
+    # url = f"https://barttorvik.com/?year=2026&sort=&hteam=&t2value=&conlimit=All&state=All&begin={formatted_date}&end=20260501&top=0&revquad=0&quad=5&venue=All&type=All&mingames=0#"
 
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(2)
-    html = driver.page_source
-#     resp = req.get(url)
-    soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find('table')
+#     driver = webdriver.Chrome()
+#     driver.get(url)
+#     time.sleep(2)
+#     html = driver.page_source
+# #     resp = req.get(url)
+#     soup = BeautifulSoup(html, 'html.parser')
+#     table = soup.find('table')
 
-    rows = []
-    for tr in table.find_all('tr')[2:]:  # Skip header row
-        cells = [td.text.strip() for td in tr.find_all('td')]
-        rows.append(cells)
+#     rows = []
+#     for tr in table.find_all('tr')[2:]:  # Skip header row
+#         cells = [td.text.strip() for td in tr.find_all('td')]
+#         rows.append(cells)
 
-    df = pd.DataFrame(rows)
+#     df = pd.DataFrame(rows)
 
-    df_filtered = df.iloc[:, :2]
+#     df_filtered = df.iloc[:, :2]
 
-#     df_filtered[1] = df_filtered[1].apply(lambda x: re.sub(r"\d+", "", x).strip())
+# #     df_filtered[1] = df_filtered[1].apply(lambda x: re.sub(r"\d+", "", x).strip())
 
-    df_filtered.columns = ['recency_rank','team']
+#     df_filtered.columns = ['recency_rank','team']
 
-    df_filtered['recency_rank'] = (
-        df_filtered['recency_rank']
-            .fillna(365)
-            .astype(int)
-    )
+#     df_filtered['recency_rank'] = (
+#         df_filtered['recency_rank']
+#             .fillna(365)
+#             .astype(int)
+#     )
 
-    df_filtered
+#     df_filtered
 
     torvik_download = 'https://barttorvik.com/2026_teamsheets.json'
 
@@ -77,41 +77,38 @@ def generate_rankings():
     # cols = pd.Series(df.columns)
     # df.columns = cols.where(~cols.duplicated(), cols + '_' + cols.groupby(cols).cumcount().astype(str))
 
-    resume_weight = .5
-    predictive_weight = .4
-    recency_weight = .1
+    resume_weight = .55
+    predictive_weight = .45
+    # recency_weight = .1
 
     # df = pd.DataFrame(df, columns = ['Team', 'Avg', 'Avg_1'])
     # df = df.rename(columns={'Team':'team', 'Avg': 'resume_metric_avg', 'Avg_1':'predictive_metric_avg'})
     # df["team"] = df["team"].apply(lambda x: re.sub(r"\d+", "", x).strip())
     # df["team"] = df["team"].str.replace(r"\b(FO|NO)\b", "", regex=True).str.strip()
-    recency_df = df_filtered
+    # recency_df = df_filtered
     # df["team"] = df["team"].str.strip()
     # recency_df = recency_df.dropna(subset=["team"])
     # recency_df["team"] = recency_df["team"].apply(lambda x: x.split("\xa0")[0])
-    df = pd.merge(df, recency_df, on="team", how ="outer")
+    # df = pd.merge(df, recency_df, on="team", how ="outer")
     # df['resume_score'] = round(((365 - df['resume_metric_avg'].astype(float))/3.64),1)
     # df['resume_rank'] = df['resume_score'].rank(ascending=False).astype(int)
-    # df['predictive_score'] = round(((365 - df['predictive_metric_avg'].astype(float))/3.64),1)
-    # df['predictive_rank'] = df['predictive_score'].rank(ascending=False).astype(int)
-    # df['cumulative_score'] = round(((df['resume_score'].astype(float) + df['predictive_score'].astype(float))/2),1)
     df['cumulative_score'] = round(((df['resume_metric_avg'].astype(float) + df['predictive_metric_avg'].astype(float))/2),1)
     # df = df.sort_values(by = 'resume_rank').reset_index()
     df['rank'] = df['cumulative_score'].rank(ascending=True, method = 'first')
-    df['recency_rank'] =df['recency_rank'].fillna(365).astype(int)
+    # df['recency_rank'] =df['recency_rank'].fillna(365).astype(int)
     # df['combined_avg'] = round(((df['resume_metric_avg'].astype(float) + df['predictive_metric_avg'].astype(float))/2),1)
-    df['combined_avg'] = round(((df['resume_metric_avg'].astype(float)/(1/resume_weight) + df['predictive_metric_avg'].astype(float)/(1/predictive_weight) + df['recency_rank'].astype(float)/(1/recency_weight))),1)
+    df['combined_avg'] = round(((df['resume_metric_avg'].astype(float)/(1/resume_weight) + df['predictive_metric_avg'].astype(float)/(1/predictive_weight))),1)
     # df['cumulative_score'] = round(((365 - df['score'])/3.64),1)
     df = df.dropna(subset=["rank"])
     df['rank'] = df['combined_avg'].rank(ascending=True, method = 'first').astype(int)
     df['team'] = df['team'].str.replace('\d+', '', regex=True)
-    cols = ['rank', 'team', 'resume_metric_avg', 'predictive_metric_avg', 'recency_rank', 'combined_avg']
+    cols = ['rank', 'team', 'resume_metric_avg', 'predictive_metric_avg', 'combined_avg'] #'recency_rank'
     df = df[cols]
 
     df = df.rename(columns={
     'resume_metric_avg': 'resume rank',
     'predictive_metric_avg': 'predictive rank',
-    'recency_rank': 'recency rank',
+    # 'recency_rank': 'recency rank',
     'combined_avg': 'composite rank'
 })
 
